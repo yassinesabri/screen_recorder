a71438080e0242fabc9c72fd63a669db1b826afa
Yassine SABRI <ysabri@leyton.com>
2020-03-31 15:53:26 +0200

    save recorded video to fileSystem

--- src/render.js
+++ src/render.js
@@ -2,11 +2,29 @@ const {desktopCapturer, remote} = require('electron');
 
 const {Menu} = remote;
 
+let mediaRecorder; // MediaRecorder instance to capture footage
+
+const recordedChunks = [];
+
 //Buttons
 const videoElement = document.querySelector('video');
+
 const startBtn = document.getElementById('startBtn');
+startBtn.onclick = e => {
+    mediaRecorder.start();
+    startBtn.classList.add('is-danger');
+    startBtn.innerText = 'Recording';
+}
+
 const stopBtn = document.getElementById('stopBtn');
+stopBtn.onclick = e => {
+    mediaRecorder.stop();
+    startBtn.classList.remove('is-danger');
+    startBtn.innerText = 'Start';
+}
+
 const videoSelectBtn = document.getElementById('videoSelectBtn');
+videoSelectBtn.onclick = getVideoSources;
 
 // get available video sources 
 async function getVideoSources() {
@@ -25,8 +43,6 @@ async function getVideoSources() {
 
     videoOptionsMenu.popup();
 }
-
-videoSelectBtn.onclick = getVideoSources;
 
 // change the videoSource window to record
 async function selectSource(source) {
@@ -47,4 +63,42 @@ async function selectSource(source) {
     // preview the stream in real time within the video element
     videoElement.srcObject = stream;
     videoElement.play();
+
+    // create Media Recorder
+    const options = {mimeType: 'video/webm; codecs=vp9'};
+    mediaRecorder = new MediaRecorder(stream, options);
+    
+    // Register event handlers
+    mediaRecorder.ondataavailable = handleDataAvailable;
+    mediaRecorder.onstop = handleStop;
+}
+
+
+function handleDataAvailable(e) {
+    recordedChunks.push(e.data);
+}
+
+const {dialog} = remote;
+const {writeFile} = require('fs');
+
+// Save video file on stop
+async function handleStop(e) {
+    const blob = new Blob(recordedChunks, {
+        type: 'video/webm; codecs=vp9'
+    });
+
+    const buffer = Buffer.from(await blob.arrayBuffer());
+
+    const {filePath} = await dialog.showSaveDialog({
+        buttonLabel: 'Save video',
+        defaultPath: `record-${Date.now()}.webm`
+    });
+
+    console.log(filePath);
+
+    if(filePath) {
+        writeFile(filePath, buffer, () => {
+            console.log('Record was saved successfully');
+        });
+    }
 }
